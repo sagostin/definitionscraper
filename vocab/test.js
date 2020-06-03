3364ea730e192683c36f30fd097cd1fbfd4ae971
Shaun Agostinho <shaunagostinho@gmail.com>
2020-06-03 16:57:34 -0700

    Added vocab folder

---
+++ vocab/test.js
@@ -0,0 +1,33 @@
+let json = {
+  'vocabulary': [{
+    'word': 'Municipal',
+    'definition': 'Relating to a city or town or its governing body.'
+  }, {
+    'word': 'Jurisdiction',
+    'definition': 'The official power to make legal decisions and judgments.'
+  }, {
+    'word': 'Detachment',
+    'definition': 'The state of being objective or aloof.'
+  }, {
+    'word': 'Mounted',
+    'definition': 'Riding an animal, typically a horse, especially for military or other duty.'
+  }, {
+    'word': 'Illicit',
+    'definition': 'Forbidden by law, rules, or custom.'
+  }, {
+    'word': 'Obstruct',
+    'definition': 'Block (an opening, path, road, etc.); be or get in the way of.'
+  }, {
+    'word': 'Sobriety',
+    'definition': 'The state of being sober.'
+  }, {
+    'word': 'Search warrant',
+    'definition': 'A legal document authorizing a police officer or other official to enter and search premises.'
+  }, {
+    'word': 'Legal aid',
+    'definition': 'Payment from public funds allowed, in cases of need, to help pay for legal advice or proceedings.'
+  }, {
+    'word': 'Excessive force',
+    'definition': 'Could not find.'
+  }]
+};
